6d44e5c24ffb19f0899f4347c139e58b5eaeb546
Orubebe Tamarah <paulorubebe@yahoo.com>
2024-02-09 23:42:09 +0100

    Add exercise 1 in hasing directory for Rust submissions

---
+++ Rust_Submissions/hashing/exercise1.rs
@@ -0,0 +1,40 @@
+/*
+Check if the Sentence Is Pangram
+
+Solution
+A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+Output: true
+Explanation: sentence contains at least one of every letter of the English alphabet.
+Example 2:
+
+Input: sentence = "leetcode"
+Output: false
+ 
+
+Constraints:
+
+1 <= sentence.length <= 1000
+sentence consists of lowercase English letters.
+
+*/
+
+fn check_if_pangram(sentence: String) -> bool {
+    use std::collections::HashSet;
+    let mut set = HashSet::new();
+        
+    for c in sentence.chars() {
+        set.insert(c);
+    }
+    set.len() == 26
+}
+
+// time complexity would be o(n) as well as the space complexity because we have to store all the unique
+// alphabets in the set

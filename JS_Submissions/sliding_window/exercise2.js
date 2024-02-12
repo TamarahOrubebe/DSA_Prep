ea870fd6883348682d5cfcc4f3096251dcc5b12b
Orubebe Tamarah <paulorubebe@yahoo.com>
2024-02-12 23:59:19 +0100

    Add exercise 9 in hashing for JS submissions

--- JS_Submissions/sliding_window/exercise2.js
+++ JS_Submissions/sliding_window/exercise2.js
@@ -2,7 +2,8 @@
 Max Consecutive Ones III
 
 Solution
-Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can
+flip at most k 0's.
 
  
 

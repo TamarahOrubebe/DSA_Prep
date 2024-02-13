ec370c7124f4311e4730f39adbec622e24147b38
Orubebe Tamarah <paulorubebe@yahoo.com>
2024-02-13 18:22:31 +0100

    Add exercise 9 in hashing for Rust submissions

--- Rust_Submissions/hashing/exercise9.rs
+++ Rust_Submissions/hashing/exercise9.rs
@@ -24,4 +24,27 @@ Explanation: The answer is "wke", with the length of 3.
 Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
  
 */
-
+use std::collections::HashMap;
+fn length_of_longest_substring(s: String) -> i32 {
+        let mut hash = HashMap::new();
+        let mut left = 0;
+        let mut ans = 0;
+        let mut vec: Vec<char> = vec![];
+        
+        for (i, char) in s.chars().enumerate() {   
+            *hash.entry(char).or_insert(0) += 1;
+            vec.push(char);
+            while hash.get(&char).unwrap() > &1 {
+                *hash.entry(vec[left]).or_insert(0) -= 1;
+                left += 1;
+            }
+            let i = i as i32;
+            let left = left as i32;
+            ans = ans.max(i - left + 1);
+               
+        
+        }
+    ans
+        
+}
+// time complexity O(n) because of an amortised inner while loop. space complexity O(k)

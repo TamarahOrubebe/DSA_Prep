2be712659c77bd9ca3c91364ab6e16d8060ad3bf
Orubebe Tamarah <paulorubebe@yahoo.com>
2024-02-15 23:16:23 +0100

    Add exercise 1 in fast and slow pointers for Rust submissions

---
+++ Rust_Submissions/linked_list/two_pointers/exercise1.rs
@@ -0,0 +1,67 @@
+/* 
+Middle of the Linked List
+
+Solution
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+Example 2:
+
+
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 100].
+1 <= Node.val <= 100
+*/
+
+
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn middle_node(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut slow = &head;
+        let mut fast = &head;
+        let mut idx = 0;
+        
+        while fast.is_some() && fast.as_ref().unwrap().next.is_some() {
+            slow = &slow.as_ref().unwrap().next;
+            fast = &fast.as_ref().unwrap().next.as_ref().unwrap().next;
+            idx += 2;
+        }
+        
+        idx += 1;
+        if idx % 2 == 0 {
+            return slow.as_ref().unwrap().next.clone();
+        }
+        
+        slow.clone()
+    }
+}

2cfd966f031cc7233125df83de77e70848127a91
Orubebe Tamarah <paulorubebe@yahoo.com>
2024-02-19 19:23:28 +0100

    Add code for exercise 3 in fast and slow pointers for Rust submissions

--- Rust_Submissions/linked_list/two_pointers/exercise3.rs
+++ Rust_Submissions/linked_list/two_pointers/exercise3.rs
@@ -26,5 +26,67 @@ The number of nodes in the list is n.
  
 
 Follow up: Could you do it in one pass?
+*/
 
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn reverse_between(mut head: Option<Box<ListNode>>, left: i32, right: i32) -> Option<Box<ListNode>> {
+        let mut curr = &mut head;
+        let left = left - 1;
+        
+        for _ in 0..left { // this takes you to the node just before the left node with curr pointing at that 
+            //node's next node which is the left node itself.
+            if let Some(node) = curr {
+                curr = &mut node.next;
+            } else {
+                // if the left is out of bounds like greater than th length of the linked list return the head
+                return head;
+            }
+        }
+        
+        // This takes ownership of the rest of the list i.e from the node at position left and hands it over to
+        // the rest variable.
+        let mut rest = curr.take();
+        let mut prev = None;
+        
+        // This loop reverses the linked list from the left node to the right node.
+        // at the end of the loop, rest points to the node after the right node.
+        for _ in left..right {
+            if let Some(mut node) = rest {
+                rest = node.next;
+                node.next = prev;
+                prev = Some(node);
+            }
+        }
+        // This updates the left node's next node to point to prev basically joining the reversed part
+        // of the list to the node before the left node.
+        *curr = prev;
 
+        // This loop now goes over the reversed part of the list to the node after the right index.
+        for _ in left..right {
+            if let Some(node) = curr {
+                curr = &mut node.next;
+            }
+        }
+        
+        // this sets the next node of the reversed end to the rest of the list completing the attachment of the
+        // reversed part to the rest of the linked list. 
+        *curr = rest;
+        head
+    }
+}

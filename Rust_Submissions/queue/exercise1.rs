0e95728c581efcac2b872e70145420d9d2666ecd
Orubebe Tamarah <paulorubebe@yahoo.com>
2024-02-25 16:09:33 +0100

    Create queue repo in Rust submissions and add exercise 1 in it

---
+++ Rust_Submissions/queue/exercise1.rs
@@ -0,0 +1,75 @@
+/*
+Moving Average from Data Stream
+
+Solution
+Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
+
+Implement the MovingAverage class:
+
+MovingAverage(int size) Initializes the object with the size of the window size.
+double next(int val) Returns the moving average of the last size values of the stream.
+ 
+
+Example 1:
+
+Input
+["MovingAverage", "next", "next", "next", "next"]
+[[3], [1], [10], [3], [5]]
+Output
+[null, 1.0, 5.5, 4.66667, 6.0]
+
+Explanation
+MovingAverage movingAverage = new MovingAverage(3);
+movingAverage.next(1); // return 1.0 = 1 / 1
+movingAverage.next(10); // return 5.5 = (1 + 10) / 2
+movingAverage.next(3); // return 4.66667 = (1 + 10 + 3) / 3
+movingAverage.next(5); // return 6.0 = (10 + 3 + 5) / 3
+ 
+
+Constraints:
+
+1 <= size <= 1000
+-105 <= val <= 105
+At most 104 calls will be made to next.
+*/
+
+use std::collections::VecDeque;
+
+struct MovingAverage {
+    size: i32,
+    total: i32,
+    deck: VecDeque<i32>
+}
+
+
+/** 
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl MovingAverage {
+
+    fn new(size: i32) -> Self {
+        Self {
+            size,
+            total: 0,
+            deck: VecDeque::new()
+        }
+    }
+    
+    fn next(&mut self, val: i32) -> f64 {
+        self.deck.push_back(val);
+        self.total += val;
+        
+        if(self.deck.len() as i32 > self.size) {
+            let num_to_sub = self.deck.pop_front().unwrap();
+            self.total -= num_to_sub;
+        }
+        self.total as f64 / self.deck.len() as f64
+    }
+}
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * let obj = MovingAverage::new(size);
+ * let ret_1: f64 = obj.next(val);
+ */

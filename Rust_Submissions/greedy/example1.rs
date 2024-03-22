ed28ef273ec800672be619090f0f66361f523f4f
Orubebe Tamarah <paulorubebe@yahoo.com>
2024-03-22 18:12:37 +0100

    Created greedy repo in Rust submssions and added example 1

---
+++ Rust_Submissions/greedy/example1.rs
@@ -0,0 +1,38 @@
+/*Example 3: 502. IPO
+
+LeetCode would like to work on some projects to increase its capital before IPO. You are given n projects where the i^th project has a profit of
+profits[i] and a minimum capital of capital[i] is needed to start it. Initially, you have w capital. When you finish a project, the profit will
+be added to your total capital. Return the max capital possible if you are allowed to do up to k projects.
+*/
+
+use std::collections::BinaryHeap;
+use std::iter::zip;
+
+fn find_maximized_capital(k: i32, mut w: i32, profits: Vec<i32>, capital: Vec<i32>) -> i32 {
+    let mut heap: BinaryHeap<i32> = BinaryHeap::new();
+    let n = profits.len() as i32;
+    let mut projects: Vec<_> = zip(capital, profits).collect();
+    projects.sort();
+    let mut i = 0;
+    
+    for _ in 0..k {
+        while i < n && projects[i as usize].0 <= w {
+            heap.push(projects[i as usize].1);
+            i += 1;
+        }
+        
+        if heap.is_empty() {
+            return w;
+        }
+        
+        w += heap.pop().unwrap();
+    }
+    w
+}
+
+
+fn main() {
+    let profits = vec![3, 1, 6, 12, 20];
+    let capital = vec![0, 0, 2, 5, 7];
+    println!("The maximized capital is {}", find_maximized_capital(3, 1, profits, capital));
+}

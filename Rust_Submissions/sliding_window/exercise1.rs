d042937e16535c4a37ac9ff1a1f1c6eae4175ba8
Orubebe Tamarah <paulorubebe@yahoo.com>
2024-04-12 17:15:29 +0100

    Modified exercise 1 in sliding window for Rust submissions

--- Rust_Submissions/sliding_window/exercise1.rs
+++ Rust_Submissions/sliding_window/exercise1.rs
@@ -27,36 +27,23 @@ n == nums.length
 
 */
 pub fn find_max_average(nums: Vec<i32>, k: i32) -> f64 {
-    let mut left: usize = 0;
-    let mut w_len = 0;
-    let mut curr = 0;
-    let mut ans: f64 = 0.0;
-    let len = nums.len() as i32;
-
+        let mut ans: f64 = 0.0;
+        let mut curr_sum = 0;
         
-    for i in 0..k {
-        curr += nums[i as usize];
-        w_len += 1;
-    }
-        
-    ans = curr as f64 / k as f64; 
-        
-    for right in k..len {
-        curr += nums[right as usize];
-        w_len += 1;
-            
-        while w_len > k {
-            curr -= nums[left];
-            w_len -= 1;
-            left += 1;
+        for i in 0..k {
+            curr_sum += nums[i as usize];
         }
-            
-        ans = f64::max(ans, curr as f64 / k as f64);
-    }
-    ans
+        
+        ans = curr_sum as f64 / k as f64;
+        
+        for i in k..nums.len() as i32 {
+            curr_sum += nums[i as usize] - nums[(i - k) as usize];
+            ans = ans.max(curr_sum as f64 / k as f64);
+        }
+        
+        ans
 }
 
-// the work done in each loop iteration is amortized constant, so this algorithm has a runtime of O(n),
-// where nn is the length of nums, and O(1) space.
+// both loops make up the total of n so this algorithm has a runtime of O(n),
+// where n is the length of nums, and O(1) space.
 
-// O(k) + amortised O(n - k) == O(n)

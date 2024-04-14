0d63c739e3d199c30cf9e139a92364c9cb877404
Orubebe Tamarah <paulorubebe@yahoo.com>
2024-04-14 08:35:14 +0100

    Added example 2 in hashing for Rust submissions

--- Rust_Submissions/hashing/example1.rs
+++ Rust_Submissions/hashing/example1.rs
@@ -8,7 +8,7 @@ target. You cannot use the same index twice.
 fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     let mut map = HashMap::new();
 
-    for (i, &num) in nums.iter().enumerate() {
+    for (i, num) in nums.iter().enumerate() {
         let complement = target - num;
         if let Some(&j) = map.get(&complement) {
             return vec![i as i32, j as i32];
